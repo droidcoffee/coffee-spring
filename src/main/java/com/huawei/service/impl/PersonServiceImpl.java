a09d2d84c235a790c0e9ac02ebe721103e872ae5
coffee <786792111@qq.com>
2016-12-19 09:07:41 +0800

    demo

---
+++ src/main/java/com/huawei/service/impl/PersonServiceImpl.java
@@ -0,0 +1,33 @@
+package com.huawei.service.impl;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.huawei.mapper.PersonMapper;
+import com.huawei.model.Person;
+import com.huawei.service.IPersonService;
+
+@Service("personService")
+public class PersonServiceImpl implements IPersonService {
+    
+    private PersonMapper personMapper;
+
+    public PersonMapper getPersonMapper() {
+        return personMapper;
+    }
+    @Autowired
+    public void setPersonMapper(PersonMapper personMapper) {
+        this.personMapper = personMapper;
+    }
+
+    @Override
+    public List<Person> loadPersons() {
+    	LoggerFactory.getLogger("test").error("test");
+        return personMapper.queryAll();
+    }
+    
+}
