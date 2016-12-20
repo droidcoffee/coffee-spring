1edcbc2fee05f210855c05f2e66c47ae8082348a
coffee <786792111@qq.com>
2016-12-20 09:07:31 +0800

    优化logback

--- src/main/java/com/huawei/service/impl/PersonServiceImpl.java
+++ src/main/java/com/huawei/service/impl/PersonServiceImpl.java
@@ -2,7 +2,6 @@ package com.huawei.service.impl;
 
 import java.util.List;
 
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -26,7 +25,7 @@ public class PersonServiceImpl implements IPersonService {
 
     @Override
     public List<Person> loadPersons() {
-    	LoggerFactory.getLogger("test").error("test");
+    	LoggerFactory.getLogger("test").error("---test----");
         return personMapper.queryAll();
     }
     
