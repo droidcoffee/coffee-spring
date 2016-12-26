94c6e09b30c0eee6902c4816f3148da1c9e5aed4
coffee <786792111@qq.com>
2016-12-26 17:49:24 +0800

    优化一个文件上传的方法

--- src/main/java/com/coffee/controller/FileController.java
+++ src/main/java/com/coffee/controller/FileController.java
@@ -2,7 +2,6 @@ package com.coffee.controller;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.Date;
 import java.util.Iterator;
 
 import javax.servlet.http.HttpServletRequest;
@@ -20,55 +19,57 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 @Controller
 public class FileController {
 
-	@RequestMapping("upload")
+	/**
+	 * 返回方法名
+	 * 
+	 * @param request
+	 * @return 文件名
+	 */
+	@RequestMapping("uploadCrash")
 	@ResponseBody
-	public String springUpload(HttpServletRequest request) throws IllegalStateException, IOException {
-		try {
-			long startTime = System.currentTimeMillis();
-			// 将当前上下文初始化给 CommonsMutipartResolver （多部分解析器）
-			CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
-			// 检查form中是否有enctype="multipart/form-data"
-			if (multipartResolver.isMultipart(request)) {
-				// 将request变成多部分request
-				MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
-				// 获取multiRequest 中所有的文件名
-				for (Iterator<String> it = multiRequest.getFileNames();it.hasNext();) {
-					// 一次遍历所有文件
-					MultipartFile file = multiRequest.getFile(it.next());
-					if (file != null) {
-						String path = "E:/springUpload" + file.getOriginalFilename();
-						// 上传
-						file.transferTo(new File(path));
+	public String springUpload(HttpServletRequest request) {
+		String resurnFileName = "";
+		// 将当前上下文初始化给 CommonsMutipartResolver （多部分解析器）
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(request.getSession().getServletContext());
+		// 检查form中是否有enctype="multipart/form-data"
+		if (multipartResolver.isMultipart(request)) {
+			// 将request变成多部分request
+			MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+			// 获取multiRequest 中所有的文件名
+			for (Iterator<String> it = multiRequest.getFileNames(); it.hasNext();) {
+				// 一次遍历所有文件
+				MultipartFile file = multiRequest.getFile(it.next());
+				if (file != null) {
+					resurnFileName = "E:/springUpload/" + file.getOriginalFilename();
+					// 上传
+					try {
+						file.transferTo(new File(resurnFileName));
+					} catch (IOException e) {
+						e.printStackTrace();
 					}
 				}
 			}
-			long endTime = System.currentTimeMillis();
-			System.out.println("方法三的运行时间：" + String.valueOf(endTime - startTime) + "ms");
-		} catch (Exception e) {
-			e.printStackTrace();
 		}
-		
-		return "111";
+		return resurnFileName;
 	}
-	
-	
-	@RequestMapping(value="uploadFile",method=RequestMethod.POST)
+
+	@RequestMapping(value = "uploadFile", method = RequestMethod.POST)
 	@ResponseBody
 	public String springUpload(@RequestParam("file") MultipartFile file) {
-		long  startTime=System.currentTimeMillis();
+		long startTime = System.currentTimeMillis();
 		try {
-	        System.out.println("fileName："+file.getOriginalFilename());
-	        String path="E:/springUpload/"+file.getOriginalFilename();
-	         
-	        File newFile=new File(path);
-	        //通过CommonsMultipartFile的方法直接写文件（注意这个时候）
+			System.out.println("fileName：" + file.getOriginalFilename());
+			String path = "E:/springUpload/" + file.getOriginalFilename();
+
+			File newFile = new File(path);
+			// 通过CommonsMultipartFile的方法直接写文件（注意这个时候）
 			file.transferTo(newFile);
-			long  endTime=System.currentTimeMillis();
-			System.out.println("方法二的运行时间："+String.valueOf(endTime-startTime)+"ms");		
+			long endTime = System.currentTimeMillis();
+			System.out.println("方法二的运行时间：" + String.valueOf(endTime - startTime) + "ms");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "111";
 	}
-	
+
 }
