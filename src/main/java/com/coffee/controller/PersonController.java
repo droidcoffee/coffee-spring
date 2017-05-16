1dcc5e4b6660474ee6ffaa5b139a4b6561fd1eee
coffee <kafei.wangt@iseastar.com>
2017-05-16 19:57:31 +0800

    不记得了

--- src/main/java/com/coffee/controller/PersonController.java
+++ src/main/java/com/coffee/controller/PersonController.java
@@ -13,22 +13,14 @@ import com.coffee.service.IPersonService;
 @Controller
 @RequestMapping("/personController")
 public class PersonController {
-    
-    private IPersonService personService;
-    
-    public IPersonService getPersonService() {
-        return personService;
-    }
- 
-    @Autowired
-    public void setPersonService(IPersonService personService) {
-        this.personService = personService;
-    }
 
-    @RequestMapping("/showPerson")
-    public String showPersons(Model model){
-        List<Person> persons = personService.loadPersons();
-        model.addAttribute("persons", persons);
-        return "showperson";
-    }
+	@Autowired
+	private IPersonService personService;
+
+	@RequestMapping("/showPerson")
+	public String showPersons(Model model) {
+		List<Person> persons = personService.loadPersons();
+		model.addAttribute("persons", persons);
+		return "showperson";
+	}
 }
